1763ed139ae7dfeddd6dea1d4b9b7819ca0b19da
Alexander Becker <alexander.becker@tum.de>
2018-04-21 11:28:53 +0200

    Add import logic

---
+++ implementation/FaceAnonymizer/Anonymizer.py
@@ -0,0 +1,23 @@
+#
+# This file includes the main functionality of the FaceAnonymizer module
+# Author: Alexander Becker
+#
+
+# TODO: import models from models submodule and load them as member variables
+
+class Anonymizer():
+
+	def __init__(self, args):
+		pass
+	
+	def train(self):
+		pass
+
+	def optimize(self):
+		pass
+
+	def save_model(self):
+		pass
+
+	def load_model(self):
+		pass
---
+++ implementation/FaceAnonymizer/__init__.py
@@ -0,0 +1,6 @@
+#
+# This is the public API to the FaceAnonymizer module
+# Author: Alexander Becker
+#
+
+from .Anonymizer import Anonymizer
