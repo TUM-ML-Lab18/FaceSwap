53cdaf9e2c244ff8cb42f2d2379ee8590ea5a8e7
sebas <sebastian.fellner@tum.de>
2018-04-21 18:03:56 +0200

    added some mockup encoder/decoder

---
+++ implementation/FaceAnonymizer/models/Encoder.py
@@ -0,0 +1,26 @@
+from torch.nn import Module
+
+
+class Encoder(Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class Decoder(Module):
+    def __init__(self, encoder):
+        super().__init__()
+        self.encoder = encoder
+
+    def forward(self):
+        latent_space = self.encoder()
+        pass
+
+
+class Autoencoder():
+    def __init__(self, Encoder, Decoder):
+        self.decoder = Decoder()
+        self.encoder1 = Encoder(self.decoder)
+        self.encoder2 = Encoder(self.decoder)
