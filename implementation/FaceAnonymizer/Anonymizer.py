deb0ca72bfbc21bd4c7dc0633a733b20bbd60cb6
Alexander Becker <alexander.becker@tum.de>
2018-04-21 12:36:59 +0200

    Add Anonymizer args

--- implementation/FaceAnonymizer/Anonymizer.py
+++ implementation/FaceAnonymizer/Anonymizer.py
@@ -3,15 +3,24 @@
 # Author: Alexander Becker
 #
 
-# TODO: import models from models submodule and load them as member variables
+import torch
+from models import Encoder, Decoder1, Decoder2
 
 class Anonymizer():
 
-	def __init__(self, args):
-		pass
+	def __init__(self, data1, data2, batch_size=50, epochs=500, learning_rate = 1e-4):
+		self.encoder = Encoder.model
+		self.decoder1 = Decoder1.model
+		self.decoder2 = Decoder2.model
+		self.lossfn = torch.nn.MSELoss(size_average=False)
+		self.data1 = data1
+		self.data2 = data2
+		self.epochs = epochs
 	
 	def train(self):
-		pass
+		for epoch in range(epochs):
+			#TODO: divide into batches
+			#TODO: build overall model (how?)
 
 	def optimize(self):
 		pass
