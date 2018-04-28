68e00de79ecbd77d1419768352deda5c16e273e5
Alexander Becker <alexander.becker@tum.de>
2018-04-28 12:24:50 +0200

    Update config

--- implementation/config.py
+++ implementation/config.py
@@ -1,5 +1,5 @@
-TRUMP_CAGE_BASE = "/nfs/students/summer-term-2018/project_2/data/FaceSwap/data"
+TRUMP_CAGE_BASE = "/nfs/students/summer-term-2018/project_2/data/FaceAnonymizer"
 TRUMP = "/trump"
 CAGE = "/cage"
 
-PROCESSED_IMAGES_FOLDER = "/nfs/students/summer-term-2018/project_2/projects/faceswap/processed_images"
+PROCESSED_IMAGES_FOLDER = "/nfs/students/summer-term-2018/project_2/data/FaceAnonymizer/processed_images"
