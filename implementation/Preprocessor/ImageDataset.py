64e6db1a342996d866cba0feea574f4773f3e44b
sebas <sebastian.fellner@tum.de>
2018-04-30 13:20:00 +0200

    refactoring

--- implementation/Preprocessor/ImageDataset.py
+++ implementation/Preprocessor/ImageDataset.py
@@ -1,42 +1,8 @@
-from pathlib import Path
-
 from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import Compose
 
 from Preprocessor.Transforms import FromPIL, ToPIL, Resize, ResizeTuple, RandomWarp, RandomTransform, ToTensor
-
-
-class ImageDataset(ImageFolder):
-
-    def __init__(self, root_dir, size_multiplicator=1):
-        """
-        :param root_dir:
-        :param transform:
-        :param target_transform:
-        :param size_multiplicator:
-        """
-        super().__init__(root_dir)
-        self.size_multiplicator = size_multiplicator
-        self.transform = Compose([
-            FromPIL(),
-            Resize(),
-            RandomTransform(),
-            RandomWarp(),
-            ResizeTuple((64, 64)),
-            ToPIL(),
-            ToTensor()
-        ])
-
-    def __len__(self):
-        return super().__len__() * self.size_multiplicator
-
-    def __getitem__(self, i):
-        """
-        :param i: index
-        :return: a tuple of (a tuple of warped, target) and (class label)
-        """
-        return super().__getitem__(i % self.size_multiplicator)
 
 
 class ImageDatesetCombined(Dataset):
