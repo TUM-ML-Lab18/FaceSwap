998070846dc7bc953dc0910d2210c94e0e280555
sebas <sebastian.fellner@tum.de>
2018-04-30 19:02:41 +0200

    better merging of output and original image

--- implementation/FaceAnonymizer/Anonymizer.py
+++ implementation/FaceAnonymizer/Anonymizer.py
@@ -1,3 +1,4 @@
+import cv2
 import shutil
 from pathlib import Path
 
@@ -38,10 +39,12 @@ class Anonymizer:
         """
         Converts the images and saves them.
         """
-        for idx, (img, _) in enumerate(self.image_dataset):
+        for idx, (oringinal_img, _) in enumerate(self.image_dataset):
             # extract information from image
-            extracted_information = self.extractor(np.array(img))
+            extracted_information = self.extractor(np.array(oringinal_img))
+
             network_input = Image.fromarray(extracted_information.image)
+            network_input.save(self.output_folder.__str__() + "/" + str(idx) + "_network_input.jpg", "JPEG")
             border = extracted_information.bounding_box
             network_input = self.to_tensor(network_input)
             extracted_width, extracted_height = border.right - border.left, border.bottom - border.top
@@ -49,13 +52,34 @@ class Anonymizer:
             # get network output
             network_output = self.model.anonymize(network_input.unsqueeze(0).cuda()).squeeze(0)
             # get it back to the cpu and get the data
-            network_output = network_output.cpu().detach()
+            network_output = self.from_tensor(network_output.cpu().detach())
+            network_output.save(self.output_folder.__str__() + "/" + str(idx) + "_network_output.jpg", "JPEG")
 
             # resize it to original dimensions
-            network_output = Resize((extracted_height, extracted_width))(self.from_tensor(network_output))
+            # i dont know why height and width have to be switched
+            network_output = Resize((extracted_height, extracted_width))(network_output)
 
-            # put the output onto the original image
-            img.paste(network_output, (border.left, border.top))
+            # get flat list of landmark tuple points
+            landmarks = np.asarray([item for sublist in [x for (_, x) in
+                                                         extracted_information.face_landmarks.items()] for item in
+                                    sublist])
+
+            # convert output to np array
+            output_np = np.asarray(network_output)
+            # use this feature map to copy the facial part of the output onto the original image
+            feature_mask = np.zeros((oringinal_img.height, oringinal_img.width))
+            head_features = cv2.convexHull(landmarks)
+            cv2.fillConvexPoly(feature_mask, head_features, 1)
+            feature_mask = feature_mask.astype(np.bool)
+
+            # generate image that is as big as the original and copy the output on the correct position
+            out_face = np.zeros((oringinal_img.height, oringinal_img.width, 3), dtype=np.uint8)
+            out_face[border.top:border.bottom, border.left: border.right, :] = output_np
+
+            # now use feature map and only copy the face onto the original image
+            oringinal_img = np.array(oringinal_img).copy()
+            oringinal_img[feature_mask] = out_face[feature_mask]
+            oringinal_img = Image.fromarray(oringinal_img)
 
             # save image
-            img.save(self.output_folder.__str__() + "/" + str(idx) + ".jpg", "JPEG")
+            oringinal_img.save(self.output_folder.__str__() + "/" + str(idx) + ".jpg", "JPEG")
