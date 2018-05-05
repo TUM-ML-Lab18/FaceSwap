f0c5cc99e5e695cca4927f8391ef1a882175535a
Alexander Becker <alexander.becker@tum.de>
2018-05-05 14:22:32 +0200

    Add BARDEM_MORGAN_BASE to main

--- implementation/main.py
+++ implementation/main.py
@@ -1,11 +1,11 @@
 from FaceAnonymizer.Trainer import Trainer
 from Preprocessor.Preprocessor import Preprocessor, deep_fake_config
-from config import MERKEL_KLUM_BASE, sebis_config
+from config import MERKEL_KLUM_BASE, BARDEM_MORGAN_BASE, sebis_config
 
 if __name__ == '__main__':
     current_config = sebis_config
 
-    p = Preprocessor(MERKEL_KLUM_BASE, current_config)
+    p = Preprocessor(BARDEM_MORGAN_BASE, current_config)
     data = p.dataset
 
     trainer = Trainer(data, current_config)
