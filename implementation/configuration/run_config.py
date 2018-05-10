aaf0efc1ec9136eee851a80aae0677c8c368cb65
Alexander Becker <alexander.becker@tum.de>
2018-05-10 12:29:30 +0200

    add alex config

--- implementation/configuration/run_config.py
+++ implementation/configuration/run_config.py
@@ -46,6 +46,39 @@ sebis_config = {'batch_size': 64,
                                                                      img_size=(128, 128)))
                 }
 
+alex_config = {'batch_size': 512,
+                'num_epoch': 5000,
+                'img_size': (128, 128),
+                'validation_freq': 20,
+                'data_loader':lambda dataset: TrainValidationLoader(dataset=dataset,
+                                           batch_size=64,
+                                           validation_size=0.2,
+                                           shuffle=True,
+                                           num_workers=12,
+                                           pin_memory=True,
+                                           drop_last=True),
+                'model': lambda: DeepFakeOriginal(
+                    encoder=lambda: Encoder(input_dim=(3, 128, 128),
+                                            latent_dim=1024,
+                                            num_convblocks=5),
+                    decoder=lambda: Decoder(input_dim=512,
+                                            num_convblocks=4),
+                    auto_encoder=AutoEncoder,
+                    loss_function=torch.nn.L1Loss(size_average=True),
+                    optimizer=lambda params: Adam(params=params, lr=1e-4),
+                    scheduler=lambda optimizer: ReduceLROnPlateau(optimizer=optimizer,
+                                                                  verbose=True,
+                                                                  patience=100,
+                                                                  cooldown=50), ),
+                'preprocessor': lambda root_folder: Preprocessor(root_folder=root_folder,
+                                                                 face_extractor=lambda: FaceExtractor(margin=0.05,
+                                                                                                      mask_type=np.bool,
+                                                                                                      mask_factor=10),
+                                                                 image_dataset=lambda path: ImageDatesetCombined(
+                                                                     dataset=path,
+                                                                     img_size=(128, 128)))
+                }
+
 landmarks_config = {'batch_size': 64,
                     'num_epoch': 5000,
                     'img_size': (128, 128),
