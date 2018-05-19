834d54605532e2e67a425742ac8d959dd1100727
Alexander Becker <alexander.becker@tum.de>
2018-05-19 14:39:47 +0200

    insert correct model path

--- implementation/main2.py
+++ implementation/main2.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model_emotion = Encoder(input_dim=(3, 128, 128), latent_dim=1024, num_convblocks=5)
-    model_emotion.load("")
+    model_emotion.load("./model/encoder.model")
 
     model_face = Encoder(input_dim=(3, 128, 128), latent_dim=1024, num_convblocks=5)
-    model_face.load("")
+    model_face.load("./model/encoder.model")
 
     loader = transforms.Compose([
         transforms.Resize(128),
