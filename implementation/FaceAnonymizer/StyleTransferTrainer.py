d9c2576929ecb0e13e73575e16e82a9519e81941
Alexander Becker <alexander.becker@tum.de>
2018-05-19 13:55:21 +0200

    Bugfix

--- implementation/FaceAnonymizer/StyleTransferTrainer.py
+++ implementation/FaceAnonymizer/StyleTransferTrainer.py
@@ -13,31 +13,45 @@ from configuration.general_config import MOST_RECENT_MODEL
 
 class StyleTransferTrainer:
 
-    def __init__(self, image, epochs, alpha, beta):
+    def __init__(self, model_emotion, model_face, image, epochs, alpha, beta):
+        self.model_emotion = model_emotion
+        self.model_face = model_face
         self.image = image
+        self.input_img = image.clone()
         self.epochs = epochs
         self.alpha = alpha
         self.beta = beta
 
+        self.loss_emotion = MSELoss(self.model_emotion(self.image).detach())
+        self.loss_face = MSELoss(self.model_face(self.image).detach())
         self.optimizer = optim.LBFGS([image.requires_grad_()])
 
     def train(self):
         print("Starting training")
+
         for i in range(self.epochs):
 
             def closure():
                 self.optimizer.zero_grad()
-                #compute and return loss
+
+                le = self.loss_emotion(self.model_emotion(self.input_img))
+                lf = self.loss_face(self.model_face(self.input_img))
+                loss = self.alpha * le + self.beta * lf
+                loss.backward()
+
+                if i % 50 == 0:
+                    print(f"[Epoch {i}] Loss: {loss}")
 
             self.optimizer.step(closure)
+            self.input_img.data.clamp_(0, 1)
 
         print("Finished training")
 
 
-class Loss(nn.Module):
+class MSELoss(nn.Module):
 
     def __init__(self, target):
-        super(Loss, self).__init__()
+        super(MSELoss, self).__init__()
         self.target = target.detach() #detach such that its not considered for autograd
 
     def forward(self, *input):
