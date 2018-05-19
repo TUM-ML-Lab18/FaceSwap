d24bc7364728f2eb2a200188aca2b623131269a8
Sebastian Fellner <ga96kug@tum.de>
2018-05-19 12:16:08 +0200

    added new models with bridge functions

--- implementation/configuration/run_config.py
+++ implementation/configuration/run_config.py
@@ -11,7 +11,8 @@ from FaceAnonymizer.models.Autoencoder import AutoEncoder
 from FaceAnonymizer.models.Decoder import Decoder, LatentDecoder
 from FaceAnonymizer.models.DeepFakeOriginal import DeepFakeOriginal
 from FaceAnonymizer.models.Encoder import Encoder
-from FaceAnonymizer.models.LatentModel import LatentModel
+from FaceAnonymizer.models.LatentModel import LatentModel, LowResAnnotationModel, HistAnnotationModel, HistModel, \
+    LowResModel
 from Preprocessor.FaceExtractor import FaceExtractor
 from Preprocessor.ImageDataset import ImageDatesetCombined, LandmarksDataset, LandmarksLowResDataset, \
     LandmarksHistDataset, LandmarksHistAnnotationsDataset, LandmarksLowResAnnotationsDataset
@@ -49,39 +50,6 @@ standart_config = {'batch_size': 64,
                    'img2latent_bridge:': lambda extracted_face, extracted_information, img_size:
                    ToTensor()(extracted_face.resize(img_size, resample=BICUBIC)).unsqueeze(0).cuda()
                    }
-
-alex_config = {'batch_size': 512,
-               'num_epoch': 5000,
-               'img_size': (128, 128),
-               'validation_freq': 20,
-               'data_loader': lambda dataset, batch_size: TrainValidationLoader(dataset=dataset,
-                                                                                batch_size=batch_size,
-                                                                                validation_size=0.2,
-                                                                                shuffle=True,
-                                                                                num_workers=12,
-                                                                                pin_memory=True,
-                                                                                drop_last=True),
-               'model': lambda: DeepFakeOriginal(
-                   encoder=lambda: Encoder(input_dim=(3, 128, 128),
-                                           latent_dim=1024,
-                                           num_convblocks=5),
-                   decoder=lambda: Decoder(input_dim=512,
-                                           num_convblocks=4),
-                   auto_encoder=AutoEncoder,
-                   loss_function=torch.nn.L1Loss(size_average=True),
-                   optimizer=lambda params: Adam(params=params, lr=1e-4),
-                   scheduler=lambda optimizer: ReduceLROnPlateau(optimizer=optimizer,
-                                                                 verbose=True,
-                                                                 patience=100,
-                                                                 cooldown=50), ),
-               'preprocessor': lambda root_folder: Preprocessor(root_folder=root_folder,
-                                                                face_extractor=lambda: FaceExtractor(margin=0.05,
-                                                                                                     mask_type=np.bool,
-                                                                                                     mask_factor=10),
-                                                                image_dataset=lambda path: ImageDatesetCombined(
-                                                                    dataset=path,
-                                                                    img_size=(128, 128)))
-               }
 
 ####### config for using only landmarks as input
 # todo move the input dimensionality of the network to somewhere als as parameter
@@ -123,36 +91,23 @@ lm_lowres_config = landmarks_config.copy()
 lm_lowres_config['dataset'] = lambda root_folder, img_size: LandmarksLowResDataset(root_folder=root_folder,
                                                                                    size_multiplicator=1,
                                                                                    target_img_size=img_size)
-lm_lowres_config['img2latent_bridge'] = lambda extracted_face, extracted_information, img_size: (
-    torch.from_numpy(
-        np.append(
-            np.reshape((np.array(extracted_information.landmarks) / extracted_information.size_fine).tolist(),
-                       -1),
-            # todo this is really ugly
-            ToTensor()(
-                extracted_face.resize(img_size, BICUBIC).resize((8, 8), BICUBIC)).numpy().flatten())
-            .astype(np.float32))
-        .unsqueeze(0).cuda()
-)
+
+lm_lowres_config['model'] = lambda img_size: LowResModel(decoder=lambda: LatentDecoder(72 * 2 + 8 * 8 * 3),
+                                                         loss_function=torch.nn.L1Loss(size_average=True),
+                                                         optimizer=lambda params: Adam(params=params, lr=1e-4),
+                                                         scheduler=lambda optimizer: ReduceLROnPlateau(
+                                                             optimizer=optimizer,
+                                                             verbose=True,
+                                                             patience=100,
+                                                             cooldown=50))
 
 ###### config for using landmarks as well as a histogram of the target as input
 lm_hist_config = landmarks_config.copy()
 lm_hist_config['dataset'] = lambda root_folder, img_size: LandmarksHistDataset(root_folder=root_folder,
                                                                                size_multiplicator=1,
                                                                                img_size=img_size)
-lm_hist_config['img2latent_bridge'] = lambda extracted_face, extracted_information, img_size: (
-    torch.from_numpy(
-        np.append(
-            np.reshape((np.array(extracted_information.landmarks) / extracted_information.size_fine).tolist(),
-                       -1),
-            # todo this is really ugly
-            np.array(extracted_face.resize(img_size, BICUBIC).histogram()).flatten() / (
-                    img_size[0] * img_size[1] * 3))
-            .astype(np.float32))
-        .unsqueeze(0).cuda()
-)
 
-lm_hist_config['model'] = lambda img_size: LatentModel(
+lm_hist_config['model'] = lambda img_size: HistModel(
     decoder=lambda: LatentDecoder(72 * 2 + 768),
     loss_function=torch.nn.L1Loss(size_average=True),
     optimizer=lambda params: Adam(params=params, lr=1e-4),
@@ -168,7 +123,7 @@ lm_hist_annotations_config['dataset'] = lambda root_folder, img_size: LandmarksH
     size_multiplicator=1,
     img_size=img_size)
 
-lm_hist_annotations_config['model'] = lambda img_size: LatentModel(
+lm_hist_annotations_config['model'] = lambda img_size: HistAnnotationModel(
     decoder=lambda: LatentDecoder(72 * 2 + 768 + 40),
     loss_function=torch.nn.L1Loss(size_average=True),
     optimizer=lambda params: Adam(params=params, lr=1e-4),
@@ -183,7 +138,7 @@ lm_lowres_annotations_config['dataset'] = lambda root_folder, img_size: Landmark
     size_multiplicator=1,
     target_img_size=img_size)
 
-lm_lowres_annotations_config['model'] = lambda img_size: LatentModel(
+lm_lowres_annotations_config['model'] = lambda img_size: LowResAnnotationModel(
     decoder=lambda: LatentDecoder(72 * 2 + 8 * 8 * 3 + 40),
     loss_function=torch.nn.L1Loss(size_average=True),
     optimizer=lambda params: Adam(params=params, lr=1e-4),
@@ -192,4 +147,4 @@ lm_lowres_annotations_config['model'] = lambda img_size: LatentModel(
                                                   patience=100,
                                                   cooldown=50))
 
-current_config = lm_lowres_config  # lm_lowres_annotations_config  # lm_hist_annotations_config  # lm_hist_config  #
+current_config = lm_hist_annotations_config
