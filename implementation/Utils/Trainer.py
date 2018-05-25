dda4f619b29e4c3da6aded724ccb631fb704f43c
sebas <sebastian.fellner@tum.de>
2018-05-25 11:48:24 +0200

    cgan works now again for multiple gpus

--- implementation/Utils/Trainer.py
+++ implementation/Utils/Trainer.py
@@ -4,15 +4,15 @@ from Utils.Logging.LoggingUtils import Logger
 from Configuration.config_general import MOST_RECENT_MODEL
 from Utils.DataSplitter import DataSplitter
 
+
 class Trainer:
     def __init__(self, root_folder, config):
         self.batch_size = config['batch_size']
+        if torch.cuda.device_count() > 1:
+            self.batch_size *= torch.cuda.device_count()
         self.dataset = config['dataset']()
         self.model = config['model'](config['img_size'])
         self.data_loader = DataSplitter(self.dataset, self.batch_size, config['num_workers'])
-
-        if torch.cuda.device_count() > 1:
-            self.batch_size *= torch.cuda.device_count()
 
         self.logger = Logger(len(self.dataset), self.model, save_model_every_nth=100,
                              shared_model_path=MOST_RECENT_MODEL)
