e44af9fd066717d5d512ef92b849cf66f65b49b4
sebas <sebastian.fellner@tum.de>
2018-05-29 16:33:09 +0200

    merkel evaluation
    Signed-off-by: sebas <sebastian.fellner@tum.de>

--- implementation/evaluate.py
+++ implementation/evaluate.py
@@ -1,6 +1,24 @@
+import json
+from pathlib import Path
+
 from PIL import Image
 
 from Evaluator.Evaluator import Evaluator
 
 if __name__ == '__main__':
-    Evaluator.evaluate_model()
+    MERKEL_DATASET = Path('/nfs/students/summer-term-2018/project_2/data/MEGA_Merkel_Trump/preprocessed/B')
+    reference_image = Image.open('/nfs/students/summer-term-2018/project_2/test/70. bundeskanzlerin-angela-merkel.jpg')
+
+    score_list = []
+
+    for folder in MERKEL_DATASET.iterdir():
+        print(f'Processing {folder}.')
+        for img in folder.iterdir():
+            img = Image.open(img)
+            score = Evaluator.evaluate_image_pair(reference_image, img)
+            score_list.append(score)
+
+    with open('./score_list.json', 'w') as h_json:
+        json.dump(score_list, h_json)
+
+    # Evaluator.evaluate_model()
