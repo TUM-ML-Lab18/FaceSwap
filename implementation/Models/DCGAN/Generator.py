bcc88dec0c74638bfd1a0d8af2006c3b5ca0e027
sebas <sebastian.fellner@tum.de>
2018-06-01 12:50:54 +0200

    added dcgan from pytorch example to repository
    Signed-off-by: sebas <sebastian.fellner@tum.de>

---
+++ implementation/Models/DCGAN/Generator.py
@@ -0,0 +1,39 @@
+from torch import nn
+
+from Models.ModelUtils.ModelUtils import CustomModule
+
+
+class Generator(CustomModule):
+    def __init__(self, ngpu, nc=3, nz=100, ngf=64):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+        self.apply(self.weights_init)
+
+    def forward(self, input):
+        if input.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
--- implementation/Models/ModelUtils/ModelUtils.py
+++ implementation/Models/ModelUtils/ModelUtils.py
@@ -1,8 +1,8 @@
 from abc import abstractmethod
 from pathlib import Path
 
-import torch.nn as nn
 import torch
+import torch.nn as nn
 
 
 class CustomModule(nn.Module):
@@ -41,17 +41,12 @@ class CustomModule(nn.Module):
 
     @staticmethod
     def weights_init(m):
-        """
-        TODO
-        :return:
-        """
         classname = m.__class__.__name__
         if classname.find('Conv') != -1:
             m.weight.data.normal_(0.0, 0.02)
         elif classname.find('BatchNorm') != -1:
             m.weight.data.normal_(1.0, 0.02)
             m.bias.data.fill_(0)
-        pass
 
 
 class CombinedModels:
