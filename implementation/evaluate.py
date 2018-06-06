94a7b832de4166d5866cea800233e0badd02937d
Alexander Becker <alexander.becker@tum.de>
2018-06-06 19:02:36 +0200

    update evaluate.py

--- implementation/evaluate.py
+++ implementation/evaluate.py
@@ -10,17 +10,5 @@ if __name__ == '__main__':
     img1 = Image.open(Path('/nfs/students/summer-term-2018/project_2/test_alex/merkel1.jpg'))
     img2 = Image.open(Path('/nfs/students/summer-term-2018/project_2/test_alex/merkel2.jpg'))
 
-    score_list = []
-
-    # for folder in MERKEL_DATASET.iterdir():
-    #    print(f'Processing {folder}.')
-    #    for img in folder.iterdir():
-    #        img = Image.open(img)
-    #        score = Evaluator.evaluate_image_pair(reference_image, img)
-    #        score_list.append(score)
-
-    # with open('./score_list.json', 'w') as h_json:
-    #    json.dump(score_list, h_json)
-
     score = Evaluator.evaluate_image_pair(img1, img2)
     print(score)
