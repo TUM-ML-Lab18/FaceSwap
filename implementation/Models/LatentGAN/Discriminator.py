9a3a3bfda5392e4b743672f7483e8318ccb15276
Alexander Becker <alexander.becker@tum.de>
2018-06-14 10:34:53 +0200

    bugfix

--- implementation/Models/LatentGAN/Discriminator.py
+++ implementation/Models/LatentGAN/Discriminator.py
@@ -1,8 +1,8 @@
 from torch import nn as nn
-import Models.CGAN.Discriminator
+from Models.CGAN.Discriminator import Discriminator as CGANDiscriminator
 
 
-class Discriminator(Models.CGAN.Discriminator):
+class Discriminator(CGANDiscriminator):
 
     def __init__(self, input_dim=(64, 64, 3), ndf=64):
         super().__init__(y_dim=0, input_dim=input_dim, ndf=ndf)
