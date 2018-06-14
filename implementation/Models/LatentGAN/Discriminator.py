6980c666bc7aea6e91dfe73f195b698861181d1a
Alexander Becker <alexander.becker@tum.de>
2018-06-14 17:02:02 +0200

    bugfix

--- implementation/Models/LatentGAN/Discriminator.py
+++ implementation/Models/LatentGAN/Discriminator.py
@@ -10,26 +10,26 @@ class Discriminator(CGANDiscriminator):
             nn.Conv2d(self.C_in, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             #
-            nn.Conv2d(ndf, ndf*2, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True)
         )
 
         # ========== Input feature vector
         self.main = nn.Sequential(
             # state size. (ndf + y_dim) x 32 x 32
-            nn.Conv2d(ndf *2, ndf * 4, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
         self.apply(self.weights_init)
